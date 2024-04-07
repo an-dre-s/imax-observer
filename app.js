82a8a7e1e5ca3604b30343013f2918bcf2538892
andre.schaeffer <andre.schaeffer@westernacher.com>
2024-04-07 11:35:16 +0200

    remove env test

--- app.js
+++ app.js
@@ -5,11 +5,6 @@ const observeImaxScreeningDates = require('./observeImaxScreeningDates');
 
 app.get("/", (req, res) => res.type('html').send(html));
 app.get('/start', () => observeImaxScreeningDates());
-app.get('/test', () => {
-  console.log(process.env.ADMIN_MAIL);
-  console.log(process.env.USER_MAILS);
-  console.log(process.env.BOT_MAIL);
-});
 
 const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
