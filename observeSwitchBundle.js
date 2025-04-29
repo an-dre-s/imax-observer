5783090752f57cc571e069d93e38fc6dd7f4d6e1
andre.schaeffer <andre.schaeffer@westernacher.com>
2025-04-29 10:31:59 +0200

    no headless

--- observeSwitchBundle.js
+++ observeSwitchBundle.js
@@ -10,7 +10,7 @@ async function observeSwitchBundle() {
     sendMail(process.env.ADMIN_MAIL, 'service started', 'https://dashboard.render.com/web/srv-co8348uv3ddc73b7ahvg/logs');
     console.log('service started');
 
-    browser = await puppeteer.launch();
+    browser = await puppeteer.launch({headless: false});
 
     page = await browser.newPage();
     page.setDefaultTimeout(15000);
